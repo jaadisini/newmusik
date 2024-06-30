224b5431a17f79c02d6d76414e413c8a4f8d1e53
jaadisini <162589971+jaadisini@users.noreply.github.com>
2024-06-30 15:08:31 +0800

    Update errors.py

--- ANNIEMUSIC/utils/decorators/errors.py
+++ ANNIEMUSIC/utils/decorators/errors.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from config import LOG_GROUP_ID
+from config import LOGGER_ID
 from ANNIEMUSIC import app
 
 
